0240531bd4eb54894a9b8a616ba45e8414d27c4d
Lilit-Moonlit <limitafternoon@gmail.com>
2026-05-12 12:49:46 +0300

    Final cleanup of migrations for Postgres

--- alembic/versions/d708d9ad9836_add_booking_comment.py
+++ alembic/versions/d708d9ad9836_add_booking_comment.py
@@ -2,14 +2,12 @@
 
 Revision ID: d708d9ad9836
 Revises: 4934208d0bf9
-Create Date: 2026-04-13 12:10:12.130844
+Create Date: 2026-04-20 12:53:14.636687
 
 """
 from typing import Sequence, Union
-
 from alembic import op
 import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = 'd708d9ad9836'
@@ -17,10 +15,8 @@ down_revision: Union[str, None] = '4934208d0bf9'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-
 def upgrade() -> None:
-    op.add_column('bookings', sa.Column('comment', sa.Text(), nullable=True))
-
+    pass
 
 def downgrade() -> None:
-    op.drop_column('bookings', 'comment')
+    pass
